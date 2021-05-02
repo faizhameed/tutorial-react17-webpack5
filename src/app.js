b1fcac09c995598db42750c9c58d2ed498e7fd7d
Faiz Hameed <faizftb@gmail.com>
2021-05-02 13:42:34 +0530

    counter

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import styles from "./app.scss";
 
 const App = () => {
+  const [counter, setCounter] = useState(1);
   return (
     <div>
-      <button className={styles.btn}>Counter</button>
+      <h1>{counter}</h1>
+      <button className={styles.btn} onClick={() => setCounter(counter + 1)}>
+        Add
+      </button>
+      <button className={styles.btn} onClick={() => setCounter(counter - 1)}>
+        Subtract
+      </button>
     </div>
   );
 };
